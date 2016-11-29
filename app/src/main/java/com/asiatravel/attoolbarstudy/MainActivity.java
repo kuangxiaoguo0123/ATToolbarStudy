ec5b38fb287225ab433ec10fa33572654df98ea2
kuangxiaoguo123 <kuangxiaoguo@asiatravel.com>
2016-11-29 21:11:50 +0800

    Add different touch state for navigationIcon

--- app/src/main/java/com/asiatravel/attoolbarstudy/MainActivity.java
+++ app/src/main/java/com/asiatravel/attoolbarstudy/MainActivity.java
@@ -7,15 +7,18 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 import com.balysv.materialmenu.MaterialMenuDrawable;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Toolbar mToolbar;
     private DrawerLayout mDrawerLayout;
     private MaterialMenuDrawable mMenuDrawable;
     private boolean isDrawerOpen;
+    private MaterialMenuDrawable.AnimationState mCurrentState;
+    private View mStateView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,10 +28,22 @@ public class MainActivity extends AppCompatActivity {
         mToolbar = (Toolbar) findViewById(R.id.tool_bar);
         setSupportActionBar(mToolbar);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mStateView = findViewById(R.id.state_view);
         mMenuDrawable = new MaterialMenuDrawable(this, Color.WHITE,
                 MaterialMenuDrawable.Stroke.THIN);
+        mCurrentState = MaterialMenuDrawable.AnimationState.BURGER_ARROW;
         initToolbar();
         initDrawerLayout();
+        initButton();
+    }
+
+    private void initButton() {
+        Button mBurgerArrowButton = (Button) mStateView.findViewById(R.id.burger_arrow_button);
+        Button mBurgerXButton = (Button) mStateView.findViewById(R.id.burger_x_button);
+        Button mBurgerCheckButton = (Button) mStateView.findViewById(R.id.burger_check_button);
+        mBurgerArrowButton.setOnClickListener(this);
+        mBurgerXButton.setOnClickListener(this);
+        mBurgerCheckButton.setOnClickListener(this);
     }
 
     private void initToolbar() {
@@ -52,7 +67,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onDrawerSlide(View drawerView, float slideOffset) {
                 mMenuDrawable.setTransformationOffset(
-                        MaterialMenuDrawable.AnimationState.BURGER_ARROW, isDrawerOpen ?
+                        mCurrentState, isDrawerOpen ?
                                 2 - slideOffset : slideOffset);
             }
 
@@ -70,5 +85,20 @@ public class MainActivity extends AppCompatActivity {
             public void onDrawerStateChanged(int newState) {
             }
         });
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.burger_arrow_button:
+                mCurrentState = MaterialMenuDrawable.AnimationState.BURGER_ARROW;
+                break;
+            case R.id.burger_x_button:
+                mCurrentState = MaterialMenuDrawable.AnimationState.BURGER_X;
+                break;
+            case R.id.burger_check_button:
+                mCurrentState = MaterialMenuDrawable.AnimationState.BURGER_CHECK;
+                break;
+        }
     }
 }
